4080da0cd1869fe33e2aecc863e24f2cc62e4edd
Fawad Ahmad <fawad7998@gmail.com>
2024-05-21 19:12:59 +0500

    sadsadsad

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,15 +8,22 @@ const Home = () => {
         <>
             <Navbar />
             <section className="text-white p-6 flex flex-col items-start justify-center h-screen bg-cover bg-center pt-24" style={{ backgroundImage: `url("/home.jpg")` }}>
-                <div className="container mx-auto text-left">
-                    <h1 className="text-5xl font-bold mb-4">Hi, I'm Jacob Aiden</h1>
+                <div className="container mx-auto mt-6  text-left">
+                    <h1 className="text-5xl font-bold mb-4">Hi, I'm Fawad Ahmad</h1>
                     <h2 className="text-3xl text-blue-500 mb-4">Frontend Developer</h2>
-                    <p className="mb-8 max-w-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet laborum, autem architecto esse in eaque eius reprehenderit atque sed maxime ex nobis dolorem nemo velit accusantium voluptatibus.</p>
+                    <p className="mb-8 max-w-lg">Passionate and detail-oriented Software Developer with expertise in various
+                        programming languages and frameworks. Skilled in creating efficient software
+                        solutions and committed to delivering top-notch work. Thrives in dynamic
+                        environments, leveraging technical know-how to solve complex problems.
+                        Experienced in front-end development, adept at integrating diverse technologies for
+                        user-friendly applications. Excels in teamwork, understanding project needs, and
+                        delivering innovative solutions that surpass expectations.
+                    </p>
                     <div>
                         <button className="bg-blue-500 text-white py-2 px-4 rounded mr-4">Hire Me</button>
                         <button className="border border-blue-500 text-blue-500 py-2 px-4 rounded">Let's Talk</button>
                     </div>
-                    <div className="flex space-x-4 mt-16 justify-start">
+                    <div className="flex space-x-4 mt-6 justify-start">
                         <a href="#" className="text-blue-500 hover:text-white rounded-full border-2 border-blue-500 w-12 h-12 flex items-center justify-center transition-colors duration-300">
                             <FontAwesomeIcon icon={faFacebookF} />
                         </a>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <header className="fixed top-0 left-0 w-full bg-transparent text-white p-6 z-50">
             <nav className="container mx-auto flex justify-between items-center">
-                <div className="text-2xl font-bold">Jacob.</div>
+                <div className="text-2xl font-bold">FA.</div>
                 <ul className="flex space-x-6">
                     <li className="hover:text-blue-500"><a href="#">Home</a></li>
                     <li className="hover:text-blue-500"><a href="#">About</a></li>
