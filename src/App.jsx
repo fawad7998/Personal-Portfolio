c32b6dc9fdc63c30b95ba126ff4d35d959125f54
Fawad Ahmad <fawad7998@gmail.com>
2024-05-25 16:44:26 +0500

    Folder Structure Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
-import Home from './components/Home';
 import {
   Route,
   Routes,
 } from "react-router-dom";
-import About from './components/About';
 import Navbar from './components/Navbar';
-import Skills from './components/Skills';
-import Experience from './components/Experience/Experience';
+import Home from "./Pages/Home"
+
 
 function App() {
 
