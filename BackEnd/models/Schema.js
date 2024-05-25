247784daeaab16b958ef518e9f14267a99f629a4
Fawad Ahmad <fawad7998@gmail.com>
2024-05-26 00:05:15 +0500

    backend added

--- BackEnd/models/Schema.js
+++ BackEnd/models/Schema.js
@@ -5,7 +5,12 @@ const schema = new mongoose.Schema({
   Projectdescription: { type: String, require: true },
   GithubLink: { type: String, require: true },
   VercelLink: { type: String, require: true },
-  ImageUrl: { type: String, require: true },
+  ImageUrl: {
+    type: String,
+    require: true,
+    default:
+      'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+  },
 });
 
 const schemas = mongoose.model('portfolioData', schema);
