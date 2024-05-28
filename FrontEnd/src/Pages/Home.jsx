ac62206b62aab11f7b16b4096412496a5907258c
Fawad Ahmad <fawad7998@gmail.com>
2024-05-29 03:19:07 +0500

    responsiveness

--- FrontEnd/src/Pages/Home.jsx
+++ FrontEnd/src/Pages/Home.jsx
@@ -15,8 +15,8 @@ const Home = () => {
     return (
         <>
             <div id='/' className='scale-90'>
-                <section className="text-white p-10 flex flex-col items-start justify-center h-screen bg-cover bg-left-top pt-20 md:pt-24 md:bg-[url('/final.png')]">
-                    <div className="container mx-auto text-left">
+                <section className="text-white p-10 flex flex-col items-center justify-center min-h-screen bg-cover bg-left-top pt-20 md:pt-24 md:bg-[url('/final.png')] md:flex-row md:justify-start">
+                    <div className="container mx-auto text-center md:text-left">
                         <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-2">Hi, I'm Fawad Ahmad</h1>
                         <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-6xl text-blue-300 mb-4">
                             <TypeAnimation
@@ -34,7 +34,7 @@ const Home = () => {
                             />
                         </h2>
                         <p className="mb-8 text-xl sm:text-2xl md:text-3xl max-w-full md:max-w-lg">with 2 years of experience</p>
-                        <div className='flex flex-wrap'>
+                        <div className='flex flex-wrap justify-center md:justify-start'>
                             <a href="/FawadCV.pdf">
                                 <button className="bg-blue-500 flex items-center text-white py-2 px-4 rounded mr-2 md:mr-4 mb-2 md:mb-0">
                                     Resume <LinkArrow className={'w-5 ml-1'} />
@@ -46,7 +46,7 @@ const Home = () => {
                                 </button>
                             </Link>
                         </div>
-                        <div className="flex space-x-2 md:space-x-4 mt-6 justify-start">
+                        <div className="flex space-x-2 md:space-x-4 mt-6 justify-center md:justify-start">
                             <a href="#" className="text-blue-500 hover:text-white rounded-full border-2 border-blue-500 w-10 h-10 md:w-12 md:h-12 flex items-center justify-center transition-colors duration-300">
                                 <FontAwesomeIcon icon={faFacebookF} />
                             </a>
--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link as ScrollLink } from 'react-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'; // Import faTimes icon
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -10,40 +10,51 @@ const Navbar = () => {
         setIsOpen(!isOpen);
     };
 
+    const cancelMenu = () => {
+        setIsOpen(false);
+    };
+
     return (
-        <header className=" scale-90 fixed  top-0 left-0 w-full bg-transparent text-white p-4 md:p-6 z-50">
+        <header className="fixed top-0 left-0 w-full bg-transparent text-white p-4 md:p-6 z-50">
             <nav className="container mx-auto flex justify-between items-center">
                 <div className="text-xl md:text-2xl font-bold cursor-pointer">
                     <ScrollLink to="/" smooth={true} duration={500}>FA.</ScrollLink>
                 </div>
                 <div className="md:hidden flex items-center">
-                    <button onClick={toggleMenu}>
-                        <FontAwesomeIcon icon={isOpen ? faTimes : faBars} className="text-2xl" />
-                    </button>
+                    {/* Conditional rendering of menu button and cross button */}
+                    {isOpen ? (
+                        <button onClick={cancelMenu}>
+                            <FontAwesomeIcon icon={faTimes} className="text-2xl" />
+                        </button>
+                    ) : (
+                        <button onClick={toggleMenu}>
+                            <FontAwesomeIcon icon={faBars} className="text-2xl" />
+                        </button>
+                    )}
                 </div>
-                <ul className={`fixed inset-0 bg-black bg-opacity-75 flex flex-col items-center justify-center space-y-6 text-xl md:static md:flex md:flex-row md:space-y-0 md:space-x-6 md:bg-transparent md:bg-opacity-0 ${isOpen ? 'flex' : 'hidden md:flex'}`}>
+                <ul className={`fixed inset-0 bg-black bg-opacity-75 backdrop-filter backdrop-blur-sm flex flex-col items-center justify-center space-y-6 text-xl md:static md:flex md:flex-row md:space-y-0 md:space-x-6 md:bg-transparent md:bg-opacity-0 ${isOpen ? 'flex' : 'hidden md:flex'}`} style={{ top: isOpen ? '0' : '-100vh', transition: 'top 0.5s' }}>
                     <li className="relative group hover:text-blue-500 cursor-pointer">
-                        <ScrollLink to="/" smooth={true} duration={500} onClick={toggleMenu}>Home</ScrollLink>
+                        <ScrollLink to="/" smooth={true} duration={500} onClick={cancelMenu}>Home</ScrollLink>
                         <span className="absolute top-6 left-0 w-full h-1 bg-sky-600 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
                     </li>
                     <li className="relative group hover:text-blue-500 cursor-pointer">
-                        <ScrollLink to="about" smooth={true} duration={500} onClick={toggleMenu}>About</ScrollLink>
+                        <ScrollLink to="about" smooth={true} duration={500} onClick={cancelMenu}>About</ScrollLink>
                         <span className="absolute top-6 left-0 w-full h-1 bg-sky-600 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
                     </li>
                     <li className="relative group hover:text-blue-500 cursor-pointer">
-                        <ScrollLink to="skills" smooth={true} duration={500} onClick={toggleMenu}>Skills</ScrollLink>
+                        <ScrollLink to="skills" smooth={true} duration={500} onClick={cancelMenu}>Skills</ScrollLink>
                         <span className="absolute top-6 left-0 w-full h-1 bg-sky-600 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
                     </li>
                     <li className="relative group hover:text-blue-500 cursor-pointer">
-                        <ScrollLink to="experience" smooth={true} duration={500} onClick={toggleMenu}>Experience</ScrollLink>
+                        <ScrollLink to="experience" smooth={true} duration={500} onClick={cancelMenu}>Experience</ScrollLink>
                         <span className="absolute top-6 left-0 w-full h-1 bg-sky-600 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
                     </li>
                     <li className="relative group hover:text-blue-500 cursor-pointer">
-                        <ScrollLink to="projects" smooth={true} duration={500} onClick={toggleMenu}>Projects</ScrollLink>
+                        <ScrollLink to="projects" smooth={true} duration={500} onClick={cancelMenu}>Projects</ScrollLink>
                         <span className="absolute top-6 left-0 w-full h-1 bg-sky-600 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
                     </li>
                     <li className="relative group hover:text-blue-500 cursor-pointer">
-                        <ScrollLink to="contact" smooth={true} duration={500} onClick={toggleMenu}>Contact</ScrollLink>
+                        <ScrollLink to="contact" smooth={true} duration={500} onClick={cancelMenu}>Contact</ScrollLink>
                         <span className="absolute top-6 left-0 w-full h-1 bg-sky-600 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out"></span>
                     </li>
                 </ul>
